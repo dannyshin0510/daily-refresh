282c22318d7800613f1c5c6f98756f5aa8d46511
dannyshin0510 <42787400+dannyshin0510@users.noreply.github.com>
2020-04-09 00:01:32 -0400

    initial api connection successful

---
+++ Desktop/dbdev/daily-project/main.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+# https://github.com/M-Media-Group/Covid-19-API - all the info for API 
+
+# Import the response as text, change  it into json, and  query for Ontario
+
+url = "https://covid-api.mmediagroup.fr/v1"
+
+querystring = {"country":"Canada"}
+
+response = requests.request("GET", url+"/cases", params=querystring)
+
+data=json.loads(response.text)
+
+print(data["Ontario"]['confirmed'], data["All"])
