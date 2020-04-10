2845900d3ecb457ef0039bac9847c84ac19d72e6
dannyshin0510 <42787400+dannyshin0510@users.noreply.github.com>
2020-04-10 18:50:50 -0400

    dynamic html mail template

--- Desktop/dbdev/daily-project/mailer.py
+++ Desktop/dbdev/daily-project/mailer.py
@@ -1,20 +1,33 @@
 import smtplib
 import config 
 import main
+from email.message import EmailMessage
+from pathlib import Path
 
-def sendEmail(subject, msg):
+msg = EmailMessage()
+msg['Subject'] = "Daily Alerts"
+msg['From'] = config.EMAIL_ADDRESS
+msg['To'] = "shindannybms@gmail.com"
+
+
+path = Path(__file__).parent / "template\mail.html"
+htmlFile = path.open('r', encoding="utf-8")
+source_code = htmlFile.read()
+htmlFile.close()
+msg.add_alternative(str(source_code), subtype='html')
+
+def sendEmail(message):
     try:
         server = smtplib.SMTP('smtp.gmail.com:587')
         server.ehlo()
         server.starttls()
         server.login(config.EMAIL_ADDRESS, config.PASSWORD)
-        message = 'Subject: {}\n\n{}'.format(subject, msg)
-        server.sendmail(config.EMAIL_ADDRESS, "shindannybms@gmail.com", message)
+        # message = 'Subject: {}\n\n{}'.format(subject, msg)
+        server.send_message(message)
         server.quit()
         print("Email sent successfully")
     except:
         print("Email failed to send")
-subject="TEST EMAIL" 
-msg="Today's current  " + str(main.number)
 
-sendEmail(subject, msg)
+
+sendEmail(msg)
